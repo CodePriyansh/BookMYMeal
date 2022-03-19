4c3087d568b90e5774e43dd79005849ac544902a
CodePriyansh <saipriyanshu786@gmail.com>
2022-03-20 01:43:14 +0530

    first

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const path=require('path');
+const adminRouter =require('./routes/admin.route');
+const userRouter = require('./routes/user.route');
+const mongoose=require('mongoose');
+
+
+
+const bodyParser = require('body-parser'); 
+app.use(express.static(path.join(__dirname,"public")));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+mongoose.connect("mongodb+srv://CodePriyanshu786:pathak123@mucluster.utw9l.mongodb.net/BookMyMeal?retryWrites=true&w=majority");
+
+
+module.exports=mongoose.connection;
+
+app.use('/api/admin',adminRouter);
+app.use('/api/user',userRouter);
+
+app.listen(3000,()=>{
+    console.log("server Started");
+})
+
